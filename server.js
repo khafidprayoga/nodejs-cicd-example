4fc60bc8d34468f2adc4978825e7ace62052d0c1
Khafid Prayoga <khafidp@pm.me>
2022-07-06 10:33:35 +0700

    add mongodb db

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const ronin = require("ronin-server")
 const mocks = require("ronin-mocks")
+const database = require("ronin-database")
 
+database.connect(process.env.CONNECTIONSTRING)
 const server = ronin.server()
 server.use("/", mocks.server(server.Router(), false, true))
 server.start()
